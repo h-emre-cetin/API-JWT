bfefaa8d69f0f84368c557ad6a23368d2d2d0900
Emre Cetin <h.emrecetin@gmail.com>
2022-06-09 23:34:02 +0300

    postSchema

---
+++ model/Post.js
@@ -0,0 +1,27 @@
+// const string = require("@hapi/joi/lib/types/string");
+// const mongoose = require("mongoose");
+
+// const postSchema = new mongoose.Schema({
+//   posts: [
+//     {
+//       title: {
+//         type: string,
+//         required: true,
+//         min: 3,
+//         max: 55,
+//       },
+//       description: {
+//         type: string,
+//         required: true,
+//         min: 3,
+//         max: 500,
+//       },
+//       createdAt: {
+//         type: Date,
+//         default: Date.now,
+//       },
+//     },
+//   ],
+// });
+
+// module.exports = mongoose.model("Post", postSchema);
